e449410803de59fb8973459407ae06bef6fab18e
NeverSettels <settels.eric@gmail.com>
2020-01-31 10:20:57 -0800

    Add change sauce and crust prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 function Pizza() {
+  this.sauce = "tomato"
+  this.crust = "regular"
   this.toppings = [];
   this.price = 0;
 }
 Pizza.prototype.addTopping = function (topping) {
   this.toppings.push(topping)
+}
+Pizza.prototype.changeSauce(sauce) = function (sauce) {
+  this.sauce = sauce
+}
+Pizza.prototype.changeCrust(sauce) = function (crust) {
+  this.crust = crust
 }
